624b9b3bff32c78d36e0fb0fe85bc2adb3621e76
Emilie Sofie Fosmo <emilie.fosmo@hotmail.com>
2026-04-27 16:35:03 +0200

    fixed load profile
    created export function getUser.
    Imported getUser to pages/profile.

--- js/utils/storage.js
+++ js/utils/storage.js
@@ -9,3 +9,12 @@ export function getAccessToken() {
 export function setUser(user) {
   localStorage.setItem("user", JSON.stringify(user));
 }
+
+export function getUser() {
+  const user = localStorage.getItem("user");
+  if (user) {
+    return JSON.parse(user);
+  } else {
+    return null;
+  }
+}
